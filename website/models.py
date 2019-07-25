3f35b9412cb98b72f5877acc0ddbe46d58165d9c
camila sales <camila.07ramones@gmail.com>
2019-07-25 17:10:35 -0300

    criar models, forms e conectar no banco

--- website/models.py
+++ website/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    GENEROS = (
+        ('M', 'Masculinio'),
+        ('F', 'Feminino'),
+        ('O', 'Outros'),
+    )
+
+    nome = models.CharField(
+        max_length=30,
+        verbose_name='Nome'
+    )
+    sobrenome = models.CharField(
+        max_length=255,
+        verbose_name='Sobrenome'
+    )
+    genero = models.CharField(
+        max_length=255,
+        verbose_name = 'Gênero',
+        choices = GENEROS
+    )
+
+    email = models.EmailField(
+        max_length=255,
+        verbose_name='E-mail'
+    )
+
+    # campo de data e hora - auto_now_add==
+    # busca de onde ta conectado a sua aplicacao e coloca a data e hora
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome+' '+self.sobrenome
--- website/views.py
+++ website/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from  website.models import Pessoa
 
 # Create your views here.
 def index(request):
-    args = {
-
-    }
+    args = {}
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        # tb é válido: pessoa.nome = request.POST['nome']
+        pessoa.nome = request.POST.get('nome')
+        pessoa.sobrenome = request.POST.get('sobrenome')
+        pessoa.genero = request.POST.get('genero')
+        pessoa.email = request.POST.get('email')
+        pessoa.save()
+        args = {'msf':'parabens'}
     return render(request,'index.html', args)
 
 def sobre(request):
